e103b2976bc630a527e488219de6352838f6cf69
sethclim <sethclim@gmail.com>
2026-03-07 00:53:07 -0500

    just static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://climenhagamusicacademy.com',
   base: "/",
-  output: "server",
-  adapter: cloudflare()
+  output: "static",
 });
